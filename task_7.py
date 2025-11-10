aaba2690b007680767b0edfe61f5831c40b86888
karina-253 <k.pinoeva@g.nsu.ru>
2025-11-11 00:45:49 +0700

    Create task_7.py

---
+++ task_7.py
@@ -0,0 +1,7 @@
+list_numbers = list(map(int, input('Введите целые'
+                              ' числа через пробел: ').split()))
+
+sum_even = sum(num for num in list_numbers if num % 2 == 0)
+sum_odd = sum(num for num in list_numbers if num % 2 != 0)
+
+print(sum_even, sum_odd)
