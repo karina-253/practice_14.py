0452c92dce32d5d5f6ccc1440c2c8f9b657c43b8
karina-253 <k.pinoeva@g.nsu.ru>
2025-11-11 00:24:58 +0700

    Create task_5.py

---
+++ task_5.py
@@ -0,0 +1,6 @@
+numbers = list(map(int, input('Введите целые'
+                              ' числа через пробел: ').split()))
+
+average_value = sum(numbers)/len(numbers) if numbers else 0
+
+print(f'Среднее значение: {average_value}')
