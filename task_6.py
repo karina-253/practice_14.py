9a60f76cb86061012e46cfe583040dfd11233143
karina-253 <k.pinoeva@g.nsu.ru>
2025-11-11 00:41:06 +0700

    Create task_6.py

---
+++ task_6.py
@@ -0,0 +1,32 @@
+from typing import List, Union
+
+def divisors(number: int) -> Union[List[int], str]:
+    '''
+    The function receives an integer and returns
+    a list of the divisors of that number.
+
+    Args:
+        number (int): One integer number
+    Returns:
+        List[int]: list of the divisors of that number.
+        str: if the number is zero.
+    '''
+    
+    if number == 0:
+        return 'Любое ненулевое число — делитель числа 0. '
+
+    list_of_divisors = []
+
+    for num in range(1, abs(number) + 1):
+        if number % num == 0:
+            list_of_divisors.append(num)
+
+    return list_of_divisors
+
+def main() -> None:
+    number = int(input('Введите целое число: '))
+    result = divisors(number)
+    print(result)
+
+if __name__ == "__main__":
+    main()
