fd095b2f29672f2c435c72ee56a09349c43f7416
karina-253 <k.pinoeva@g.nsu.ru>
2025-11-11 00:52:43 +0700

    Create task_8.py

---
+++ task_8.py
@@ -0,0 +1,26 @@
+def transformations(text: str) -> str:
+    '''
+    The function Converts a string into a list of characters,
+     sorts it, and returns the string
+
+    Args:
+        text (str): String
+    Returns:
+        str: returns the string received after transformations.
+    '''
+
+    list_of_chars = list(text)
+
+    list_of_chars.sort()
+
+    list_in_str = ''.join(list_of_chars)
+
+    return list_in_str
+
+def main() -> None:
+    input_str = input('Введите строку: ')
+    result = transformations(input_str)
+    print(result)
+
+if __name__ == "__main__":
+    main()
