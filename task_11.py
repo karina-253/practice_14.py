c8ced9a9ce3c6930786672a566816d423b20380c
karina-253 <k.pinoeva@g.nsu.ru>
2025-11-11 09:57:41 +0700

    Update task_11.py

--- task_11.py
+++ task_11.py
@@ -1,14 +1,17 @@
-lst_1 = list(map(int, input('"Введите числа через пробел:').split()))
-command = input('Введите команду: "R"+цифра для сдвига вправо'
-                'или "L"+цифра для сдвига влево:')
+try:
+    lst_1 = list(map(int, input('Введите числа через пробел:').split()))
+    command = input('Введите команду: "R"+цифра для сдвига вправо'
+                    'или "L"+цифра для сдвига влево:')
 
-side = command[0]
-shift = int(command[1:])
+    side = command[0]
+    shift = int(command[1:])
 
-if side == 'R':
-    lst_1 = lst_1[-shift:] + lst_1[:-shift]
-    print(lst_1)
+    if side == 'R':
+        lst_1 = lst_1[-shift:] + lst_1[:-shift]
+        print(lst_1)
 
-elif side == 'L':
-    lst_1 = lst_1[shift:] + lst_1[:shift]
-    print(lst_1)
+    elif side == 'L':
+        lst_1 = lst_1[shift:] + lst_1[:shift]
+        print(lst_1)
+except ValueError:
+    print('Ошибка ввода!')
