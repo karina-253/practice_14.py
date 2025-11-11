0905406127ad787badea3ab89c2222bf611d682e
karina-253 <k.pinoeva@g.nsu.ru>
2025-11-11 09:48:59 +0700

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -1,6 +1,9 @@
-numbers = list(map(int, input('Введите целые'
-                              ' числа через пробел: ').split()))
+try:
+    numbers = list(map(int, input('Введите целые'
+                                  ' числа через пробел: ').split()))
 
-average_value = sum(numbers)/len(numbers) if numbers else 0
+    average_value = sum(numbers) / len(numbers) if numbers else 0
 
-print(f'Среднее значение: {average_value}')
+    print(f'Среднее значение: {average_value}')
+except ValueError:
+    print('Ошибка: вводите только целые числа!')
