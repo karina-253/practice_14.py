0eb7302827c0fee0ad446b3ab0caca5bfa40aaf0
karina-253 <k.pinoeva@g.nsu.ru>
2025-11-11 09:38:20 +0700

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,11 +1,14 @@
-numbers = list(map(int, input('Введите 10 целых чисел: ').split()))
+try:
+    numbers = list(map(int, input('Введите 10 целых чисел через пробел: ').split()))
 
-if len(numbers) != 10:
-    print('Введите ровно 10 чисел!')
-else:
-    new_list = []
+    if len(numbers) != 10:
+        print('Ошибка: нужно ввести ровно 10 чисел!')
+    else:
+        new_list = []
+        for i in range(1, 9):
+            new_list.append(numbers[i - 1] + numbers[i + 1])
+        print('Результат:', new_list)
 
-    for i in range(1, 9):
-        new_list.append(numbers[i-1]+numbers[i+1])
-
-    print(new_list)
+except ValueError:
+    print('Ошибка: вводите только целые числа!')
+    
