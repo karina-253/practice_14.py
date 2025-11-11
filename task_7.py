1ce2a2e19bf0ac2623a10e3a40d087f625496837
karina-253 <k.pinoeva@g.nsu.ru>
2025-11-11 09:52:29 +0700

    Update task_7.py

--- task_7.py
+++ task_7.py
@@ -1,7 +1,10 @@
-list_numbers = list(map(int, input('Введите целые'
-                              ' числа через пробел: ').split()))
+try:
+    list_numbers = list(map(int, input('Введите целые'
+                                       ' числа через пробел: ').split()))
 
-sum_even = sum(num for num in list_numbers if num % 2 == 0)
-sum_odd = sum(num for num in list_numbers if num % 2 != 0)
+    sum_even = sum(num for num in list_numbers if num % 2 == 0)
+    sum_odd = sum(num for num in list_numbers if num % 2 != 0)
 
-print(sum_even, sum_odd)
+    print(sum_even, sum_odd)
+except ValueError:
+    print('Ошибка: вводите только целые числа!')
